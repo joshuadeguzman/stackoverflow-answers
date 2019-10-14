967e9335129477629ef98e33323a62e77de121b7
Joshua de Guzman <devjoshuadeguzman@gmail.com>
2019-10-14 18:02:00 +0800

    Add option 1 answer rxjava force stop

--- kotlin-gradle/src/main/kotlin/RxJava.kt
+++ kotlin-gradle/src/main/kotlin/RxJava.kt
@@ -1,7 +1,9 @@
 import io.reactivex.Observable
+import io.reactivex.subjects.PublishSubject
 
 fun main(args: Array<String>) {
-    testAnswer1()
+    // testAnswer1()
+    testAnswer2()
 }
 
 // https://stackoverflow.com/questions/54281902/rxjava-observable-create-not-emitting-events-for-switchifemptyobservable/54284694#54284694
@@ -31,4 +33,30 @@ fun testAnswer1() {
         }, {
             println(it)
         })
+}
+
+// https://stackoverflow.com/questions/54357888/rxjava-how-to-stop-publishsubject-from-publishing-even-if-onnext-is-called/54362033#54362033
+fun testAnswer2() {
+    val maxEmittedItemCount = 10
+    var currentEmittedItemCount = 0
+    val someStringValue = "Some observable" // Create whatever observable you have
+    val publishSubject = PublishSubject.create<String>()
+
+    publishSubject.subscribe({
+        currentEmittedItemCount++
+        println(it)
+    }, {
+        println(it)
+    })
+
+    while (currentEmittedItemCount != maxEmittedItemCount) {
+        // Print indication that the loop is still running
+        println("Still looping")
+
+        // Publish value on the subject
+        publishSubject.onNext(someStringValue)
+
+        // Test flag for trigger
+        if (currentEmittedItemCount == maxEmittedItemCount) publishSubject.onComplete()
+    }
 }
