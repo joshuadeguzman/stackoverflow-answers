774f3274dd003195c5bb4715061ab0ee54ad0fbf
jdeguzman <devjoshuadeguzman@gmail.com>
2019-11-27 19:16:58 +0800

    Add gesture answer

---
+++ flutter/lib/answer2/gesture.dart
@@ -0,0 +1,14 @@
+// Copyright 2019 Joshua de Guzman (https://joshuadeguzman.github.io). All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+
+// SO Link: https://stackoverflow.com/questions/59067231/how-to-get-data-after-end-drag-on-my-color-picker/59068939#59068939
+// return GestureDetector(
+//   // Might worth noting, review the behavior of your choice onPanDown onPanEnd
+//   onPanDown: (details) => handleTouch(details.globalPosition, context),
+//   // Depends if you still need this.
+//   // onPanStart: (details) => handleTouch(details.globalPosition, context),
+//   onPanEnd: (details) => handleTouch(details.globalPosition, context),
+//   child: Stack(children: [frame, content, thumb]),
+// );
