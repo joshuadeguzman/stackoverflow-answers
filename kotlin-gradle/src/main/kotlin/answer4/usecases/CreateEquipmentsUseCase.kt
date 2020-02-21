a4afa81f0eaa1f8181d1e4bd96a50ad5690e0638
jdeguzman <devjoshuadeguzman@gmail.com>
2020-02-21 15:05:18 +0800

    Update rxjava operator

--- kotlin-gradle/src/main/kotlin/answer4/usecases/CreateEquipmentsUseCase.kt
+++ kotlin-gradle/src/main/kotlin/answer4/usecases/CreateEquipmentsUseCase.kt
@@ -2,7 +2,6 @@ package answer4.usecases
 
 import answer4.models.Equipment
 import io.reactivex.Observable
-import io.reactivex.Single
 
 interface ICreateEquipmentsUseCase {
     operator fun invoke(equipments: MutableList<Equipment>): Observable<List<Equipment>?>
@@ -11,34 +10,29 @@ interface ICreateEquipmentsUseCase {
 class CreateEquipmentsUseCase : ICreateEquipmentsUseCase {
     override fun invoke(equipments: MutableList<Equipment>): Observable<List<Equipment>?> {
         return Observable.create<List<Equipment>> { emitter ->
-            val serverEquipments: MutableList<Equipment> = mutableListOf()
             if (!emitter.isDisposed) {
-                equipments.forEachIndexed { index, equipment ->
-                    // I need to send a post request in the server to create each of the equipment
-                    // And expect for the list of equipments with server IDs
-                    createEquipment(equipment)
-                        .subscribe({
-                            println("Item serverId: " + it.serverId)
-                            println("Item name: " + it.name)
-                            serverEquipments.add(it)
-
-                            if (equipments.size == index - 1) {
-                                emitter.onNext(serverEquipments)
-                                emitter.onComplete()
-                            }
-                        }, {
-                            println("Error: " + it.localizedMessage)
-                            emitter.onError(it)
-                        })
-                }
+                // I need to send a post request in the server to create each of the equipment
+                // And expect for the list of equipments with server IDs
+                Observable.fromIterable(equipments)
+                    .flatMap { equipment ->
+                        createEquipment(equipment)
+                    }
+                    .toList()
+                    .subscribe({
+                        emitter.onNext(it)
+                        emitter.onComplete()
+                    }, {
+                        println("Error: " + it.localizedMessage)
+                        emitter.onError(it)
+                    })
             }
         }
     }
 
     // Mock server request returning an updated Equipment object
-    private fun createEquipment(equipment: Equipment): Single<Equipment> {
+    private fun createEquipment(equipment: Equipment): Observable<Equipment> {
         equipment.serverId = 100
         // return Single.error(Exception("Error"))
-        return Single.just(equipment)
+        return Observable.just(equipment)
     }
 }
